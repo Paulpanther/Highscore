dfb8485ccdd6d85b59fa88710e70e76b19fa6356
Paulpanther <paul.methfessel@web.de>
2020-08-25 16:50:18 +0200

    Add Vue

---
+++ website/src/App.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  h1 Vue hello
+</template>
+
+<script lang="ts">
+  import Vue from "vue";
+  import Component from "vue-class-component";
+
+  @Component
+  export default class App extends Vue {
+  }
+</script>
+
+<style lang="sass">
+</style>
+
+<style lang="sass" scoped>
+</style>
---
+++ website/src/components/ListEntry.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+
+</template>
+
+<script lang="ts">
+  import Vue from "vue";
+  import Component from "vue-class-component";
+
+  @Component
+  export default class ListEntry extends Vue {
+
+  }
+</script>
+
+<style lang="sass" scoped>
+
+</style>
